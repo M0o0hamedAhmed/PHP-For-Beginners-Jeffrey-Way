fcacf060d2d0beee90d81e6028cfbccee9096a4e
M0o0hamedAhmed <mabolaumon@gmail.com>
2024-03-14 01:14:12 +0200

    Welcome to section two! While section one focused entirely on the initial fundamentals of PHP, now, we'll move on and learn what it looks like to build a typical PHP application with a MySQL database.

---
+++ about.php
@@ -0,0 +1,7 @@
+
+<?php
+
+require "about.view.php"
+
+?>
+
--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 
 <?php
 
-$business = [
-    "name" => "LAMON",
-    "cost" => 76,
-    "categories" => ["PHP","JS","Java"]
-];
-
 require "index.view.php"
 
 ?>
