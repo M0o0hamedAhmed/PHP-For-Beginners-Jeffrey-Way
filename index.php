09c38fc57d23d55ecc1bbeb19d7a8f94aa1a7813
M0o0hamedAhmed <mabolaumon@gmail.com>
2024-03-13 17:58:38 +0200

    Create an array of any three usernames - perhaps for a  'Top Performing Users' section of your website. Then, create a loop that displays each username within a list item.

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php $title = "Top Performing Users"; ?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -5,22 +6,23 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Your First PHP Tag Assignment</title>
+    <title><?= $title ?></title>
 </head>
 <body>
-<h1>Recommended Book</h1>
+<h1> <?= $title ?> </h1>
         <?php
-                $books = [
-                    "First Book",
-                    "Second Book",
-                    "Third Book",
+                $names = [
+                    "Mohamed",
+                    "Ahmed",
+                    "Ali",
+                    "Omar",
                 ];
         ?>
 
 
     <ul>
-        <?php foreach ($books as $book) : ?>
-            <li> <?= $book ?>  </li>
+        <?php foreach ($names as $name) : ?>
+            <li> <?= $name ?>  </li>
         <?php endforeach; ?>
     </ul>
 </body>
