f656b0d2977bcafb881a28ae44e2c659b514122e
M0o0hamedAhmed <mabolaumon@gmail.com>
2024-03-14 00:18:41 +0200

    Update your book filtering logic from this episode to only display books that were first published between the years 1950 and 2020. Hint - the PHP equivalent of

--- index.php
+++ index.php
@@ -30,6 +30,23 @@ $movies = [
         "author" => "Ali",
         "purchaseUrl" => "https://example.com",
         "releaseYear" => 2004
+    ], [
+        "name" => "First Movie",
+        "author" => "Mohamed",
+        "purchaseUrl" => "https://example.com",
+        "releaseYear" => 2002
+    ],
+    [
+        "name" => "Second Movie",
+        "author" => "Ahmed",
+        "purchaseUrl" => "https://example.com",
+        "releaseYear" => 1940
+    ],
+    [
+        "name" => "Third Movie",
+        "author" => "Ali",
+        "purchaseUrl" => "https://example.com",
+        "releaseYear" => 2024
     ]
 ];
 
@@ -45,11 +62,11 @@ function filter($movies, $fn)
 }
 
 $filteredMovies = filter($movies, function ($movie) {
-    return $movie['releaseYear'] >= 2003;
+    return   $movie['releaseYear'] >= 1950  && $movie['releaseYear'] <= 2020;
 });
-$filteredMovies = array_filter($movies, function ($movie) {
-    return $movie['releaseYear'] >= 2003;
-});
+//$filteredMovies = array_filter($movies, function ($movie) {
+//    return $movie['releaseYear'] >= 2003;
+//});
 
 ?>
 
