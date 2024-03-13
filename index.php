7a5f8310e72c1ebb2b762d775aeabf0dcdf78711
M0o0hamedAhmed <mabolaumon@gmail.com>
2024-03-13 17:28:23 +0200

    Create a paragraph that uses PHP to echo any basic sentence of your choosing. Practice typing the opening and closing <?php tags.

--- index.php
+++ index.php
@@ -5,14 +5,13 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Your First PHP Tag Assignment</title>
 </head>
 <body>
-
-    <h1>
+    <p>
         <?php
-            echo "Hello World";
+            echo  "Create a paragraph that uses PHP to echo any basic sentence of your choosing. Practice typing the opening and closing <?php tags.";
         ?>
-    </h1>
+    </p>
 </body>
 </html>
