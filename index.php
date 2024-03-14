e76b6de6fa7db402cd0e9472d8ad13250d6e296f
M0o0hamedAhmed <mabolaumon@gmail.com>
2024-03-14 05:30:21 +0200

    PDO_First_Steps_Assignment

--- index.php
+++ index.php
@@ -7,15 +7,13 @@ $dsn = "mysql:host=localhost;user=root;dbname=store;charset=utf8mb4";
 
 $pdo = new PDO($dsn);
 
- $statement = $pdo->prepare('select * from categories');
+ $statement = $pdo->prepare('select * from categories where id = 1');
  $statement->execute();
 
- $categories = $statement->fetchAll(PDO::FETCH_ASSOC);
+ $category = $statement->fetch(PDO::FETCH_ASSOC);
 
 
- foreach ($categories as $category) {
     echo "<li>{$category['name']} </li>";
- }
 
 
 ?>
